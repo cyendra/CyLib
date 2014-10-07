b04d45f16505c061c36e892b89261ea1bfb22ae4
cyendra <g1483335912@gmail.com>
2014-10-07 19:50:59 +0800

    cons

--- Cy_Lib/Limits.h
+++ Cy_Lib/Limits.h
@@ -1,7 +1,23 @@
 #pragma once
 
 #include <limits>
+#include <cstdio>
+
+class Gao{
+public:
+	Gao(){}
+	Gao(const Gao&) {
+		puts("Cons");
+	}
+	Gao& operator=(const Gao&){
+		puts("===");
+		return *this;
+	}
+};
 
 void SomeLimits() {
-
+	Gao g1;
+	Gao g2 = g1;
+	Gao g3(g2);
+	g1 = g3;
 }
