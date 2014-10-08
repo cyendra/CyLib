a7eae6b7cb8f00f865e4268edb118887a8a8baf9
cyendra <g1483335912@gmail.com>
2014-10-08 18:40:27 +0800

    virtual

--- Cy_Lib/Limits.h
+++ Cy_Lib/Limits.h
@@ -3,21 +3,71 @@
 #include <limits>
 #include <cstdio>
 
-class Gao{
+class Base {
 public:
-	Gao(){}
-	Gao(const Gao&) {
-		puts("Cons");
+	virtual void gao1() = 0;
+	virtual void gao2() = 0;
+	void gao3() {
+		printf("wwwww Gao3\n");
 	}
-	Gao& operator=(const Gao&){
-		puts("===");
-		return *this;
+	void gao4() {
+		printf("wwwww Gao4\n");
+	}
+};
+
+class Use : public Base {
+public:
+	virtual void gao1() {
+		printf("Test Gao1\n");
+	}	
+	void gao2() {
+		printf("Test Gao2\n");
+	}
+	void gao3() {
+		printf("Test Gao3\n");
+	}
+	virtual void gao4() {
+		printf("Test Gao4\n");
+	}
+};
+
+class Use2 : public Use {
+public:
+	void gao1() {
+		printf("Use Gao1\n");
+	}
+	void gao2() {
+		printf("Use Gao2\n");
+	}
+	void gao3() {
+		printf("Use Gao3\n");
+	}
+	void gao4() {
+		printf("Use Gao4\n");
 	}
 };
 
 void SomeLimits() {
-	Gao g1;
-	Gao g2 = g1;
-	Gao g3(g2);
-	g1 = g3;
+	Use s1;
+	Use2 s2;
+	Base *p = &s1;
+	p->gao1();
+	p->gao2();
+	p->gao3();
+	p->gao4();
+	p = &s2;
+	p->gao1();
+	p->gao2();
+	p->gao3();
+	p->gao4();
+	Use* q = &s1;
+	q->gao1();
+	q->gao2();
+	q->gao3();
+	q->gao4();
+	q = &s2;
+	q->gao1();
+	q->gao2();
+	q->gao3();
+	q->gao4();
 }
